ed8a5f5bad67ec9cefcb8e6ff1e5898486df542e
Dennis Aguila <purrfectbnb@gmail.com>
2025-06-26 09:45:22 +1200

    Add missing React files

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Purrfect Stays</h1>
+        <p>Welcome to your React app!</p>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
